819bcbcaae82082638072f7619d08f9b4192d0f7
Robin Roevens <robin.roevens@gmail.com>
2019-04-03 21:16:15 +0200

    Use ESP32 wifi library on ESP32

--- arduino/miflo/miflo.ino
+++ arduino/miflo/miflo.ino
@@ -23,7 +23,11 @@ int noteDurations[] = {
 
 #include "settings.h"
 
+#if defined(ESP8266)
 #include <ESP8266WiFi.h>
+#elif defined(ESP32)
+#include <WiFi.h>
+#endif
 WiFiClient espClient;
 
 #include <PubSubClient.h>
